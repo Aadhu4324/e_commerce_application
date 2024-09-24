c5f3bc01316947a13d31d38fd1271902d42fcbbf
Aadhu4324 <adarshahamed70@gmail.com>
2024-09-25 00:26:10 +0530

    Second Commit

--- lib/screens/screen_favrite.dart
+++ lib/screens/screen_favrite.dart
@@ -1,10 +1,57 @@
+import 'package:e_commerce_application/services/cart_favourite_services.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ScreenFavrite extends StatelessWidget {
   const ScreenFavrite({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _favouritecartServices = Provider.of<CartFavouriteServices>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Favorite Items"),
+        centerTitle: true,
+        backgroundColor: Colors.pink.shade300,
+      ),
+      body: Consumer<CartFavouriteServices>(
+        builder: (context, value, child) {
+          if (value.favList.isEmpty) {
+            return Center(
+              child: Text("No Item Availble"),
+            );
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ListView.separated(
+                  itemBuilder: (context, index) => Row(
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            child: Image.asset(
+                              value.favList[index].image,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(value.favList[index].title),
+                            ],
+                          )
+                        ],
+                      ),
+                  separatorBuilder: (context, index) => Divider(),
+                  itemCount: value.favList.length),
+            );
+          }
+        },
+      ),
+    );
   }
 }
